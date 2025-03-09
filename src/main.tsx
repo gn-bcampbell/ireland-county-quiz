cba2ef9cd9da2eed781c74fec9bd7024c62adaf2
Brendan <b.campbell787@gmail.com>
2025-03-09 08:23:23 +0000

    [Analytics]

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Test from "@/pages/test.tsx";
-import "./i18n"; // Import the i18n configuration
+import "./i18n";
+import {Analytics} from "@vercel/analytics/react"; // Import the i18n configuration
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
